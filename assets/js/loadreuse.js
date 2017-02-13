ebd906695f29a0d38b0c5d8c3d89b08c95ba5c37
Ryan O'Day <romanguy722@gmail.com>
2017-02-12 22:12:48 -0500

    Attempt fix of menu

--- assets/js/loadreuse.js
+++ assets/js/loadreuse.js
@@ -1,4 +1,27 @@
 $( document ).ready(function() {
     $("#headeritems").load("reuse/headeritems.html");
     $("#footer").load("reuse/footer.html");
+    loadScripts(["assets/js/main.js"],noop());
 });
+
+function loadScripts(array,callback){
+    var loader = function(src,handler){
+        var script = document.createElement("script");
+        script.src = src;
+        script.onload = script.onreadystatechange = function(){
+            script.onreadystatechange = script.onload = null;
+            handler();
+        }
+        var head = document.getElementsByTagName("head")[0];
+        (head || document.body).appendChild( script );
+    };
+    (function run(){
+        if(array.length!=0){
+            loader(array.shift(), run);
+        }else{
+            callback && callback();
+        }
+    })();
+}
+
+function noop(){}
